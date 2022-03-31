295af064e481c43e59fd49bd4b7124368ea2723e
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-03-31 06:58:17 +0200

    Create index component

--- src/app/recipe/index/index.component.ts
+++ src/app/recipe/index/index.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RecipeService } from '../recipe.service';
+import { Recipe } from '../recipe';
+
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
 })
 export class IndexComponent implements OnInit {
+  recipes: Recipe[] = [];
 
-  constructor() { }
+  constructor(public recipeService: RecipeService) {}
+
+  /* ngOnInit(): void {
+    this.recipeService.getAll().subscribe((data: RecipeAPIdata) => {
+      this.recipes[2] = data.hits[2].recipe;
+      console.log(this.recipes);
+    });
+  } */
 
   ngOnInit(): void {
+    this.recipeService.getAll().subscribe((data: Recipe[]) => {
+      this.recipes = data;
+      console.log(this.recipes);
+    });
   }
-
 }
