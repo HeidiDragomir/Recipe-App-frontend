8b7d6da00d36b271fc08365893864c4224712984
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-04-08 09:21:31 +0200

    Create recipelist edit component

---
+++ src/app/recipe-list/edit/edit.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RecipeList } from '../recipe-list';
+import { RecipeListService } from '../recipe-list.service';
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.css'],
+})
+export class EditComponent implements OnInit {
+  id!: number;
+  recipelist!: RecipeList;
+  form!: FormGroup;
+
+  constructor(
+    public recipeListService: RecipeListService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['recipelistId'];
+    this.recipeListService.find(this.id).subscribe((data: RecipeList[]) => {
+      this.recipelist = data[0];
+    });
+
+    this.form = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  submit() {
+    console.log(this.form.value);
+    this.recipeListService
+      .update(this.id, this.form.value)
+      .subscribe((res: any) => {
+        console.log('List updated successfully!');
+        this.router.navigateByUrl('recipelist/index');
+      });
+  }
+}
