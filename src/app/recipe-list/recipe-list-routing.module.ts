1e930eca756573a1a57cf08d18e1912e2b2dc685
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-04-08 09:22:49 +0200

    Create recipelist routes

---
+++ src/app/recipe-list/recipe-list-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../shared/auth.guard';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { IndexComponent } from './index/index.component';
+import { ViewComponent } from './view/view.component';
+
+const routes: Routes = [
+  { path: 'recipelist', redirectTo: 'recipelist/index', pathMatch: 'full' },
+  
+  {
+    path: 'recipelist/:recipelistId/view',
+    component: ViewComponent,
+    canActivate: [AuthGuard],
+  },
+
+  {
+    path: 'recipelist/create',
+    component: CreateComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'recipelist/:recipelistId/edit',
+    component: EditComponent,
+    canActivate: [AuthGuard],
+  },
+
+  {
+    path: 'recipelist/index',
+    component: IndexComponent,
+    canActivate: [AuthGuard],
+  },
+  /*  { path: 'recipes/:recipeId/view', component: RecipeDetailsComponent }, */
+];
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class RecipeListRoutingModule {}
